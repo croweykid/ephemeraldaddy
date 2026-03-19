98ea920a196b8e42efe429be89105ad9bf7d2037
Croweykid <113377162+croweykid@users.noreply.github.com>
2026-03-19 12:01:18 -0400

    Fix Database View letter-jump crash on PySide6

--- ephemeraldaddy/gui/app.py
+++ ephemeraldaddy/gui/app.py
@@ -1877,7 +1877,7 @@ class ChartListWidget(QListWidget):
                 self.clearSelection()
                 item.setSelected(True)
                 self.setCurrentItem(item)
-                self.scrollToItem(item, QAbstractItemView.PositionAtCenter)
+                self.scrollToItem(item, QAbstractItemView.ScrollHint.PositionAtCenter)
                 return True
         return False
 
@@ -9082,7 +9082,7 @@ class ManageChartsDialog(DatabaseAnalyticsChartsMixin, QDialog):
                 self.list_widget.clearSelection()
                 item.setSelected(True)
                 self.list_widget.setCurrentItem(item)
-                self.list_widget.scrollToItem(item, QAbstractItemView.PositionAtCenter)
+                self.list_widget.scrollToItem(item, QAbstractItemView.ScrollHint.PositionAtCenter)
                 return True
         return False
 
