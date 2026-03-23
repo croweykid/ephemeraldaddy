17798c370c520dba2d3c873a93fc4d78afbbefd8
Croweykid <113377162+croweykid@users.noreply.github.com>
2026-03-23 19:39:35 -0400

    Add chart tags metadata with UI and searchable filters

--- ephemeraldaddy/core/chart.py
+++ ephemeraldaddy/core/chart.py
@@ -16,6 +16,7 @@ class Chart:
         self.birth_place = None
         self.sentiments = []
         self.relationship_types = []
+        self.tags = []
         self.comments = ""
         self.positive_sentiment_intensity = 1
         self.negative_sentiment_intensity = 1
@@ -86,6 +87,7 @@ class Chart:
             "lon": self.lon,
             "sentiments": self.sentiments,
             "relationship_types": self.relationship_types,
+            "tags": self.tags,
             "comments": self.comments,
             "chart_type": self.chart_type,
             "source": self.chart_type,
