aa63837066ce0a174deaa60e3bf1f03c49429cbc
Croweykid <113377162+croweykid@users.noreply.github.com>
2026-04-06 14:06:43 -0400

    updated definitions

--- ephemeraldaddy/gui/app.py
+++ ephemeraldaddy/gui/app.py
@@ -9163,7 +9163,7 @@ class ManageChartsDialog(DatabaseAnalyticsChartsMixin, QDialog):
                     database_planet_counts=database_human_design_counts,
                     loaded_charts=loaded_charts,
                     labels=human_design_labels,
-                    height_scale=2 if human_design_mode in {"hd_gates", "hd_defined_centers"} else 1.0,
+                    height_scale=2 if human_design_mode in {"hd_gates"} else 1.0,
                 )
                 self._clear_layout(self.human_design_chart_layout)
                 self.human_design_chart_layout.addWidget(
