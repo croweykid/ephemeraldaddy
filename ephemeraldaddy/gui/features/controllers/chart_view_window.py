e8e27c410b1d8005894850d67fcec747cd1cb517
Croweykid <113377162+croweykid@users.noreply.github.com>
2026-04-25 09:03:22 -0400

    Update chart_view_window.py

--- ephemeraldaddy/gui/features/controllers/chart_view_window.py
+++ ephemeraldaddy/gui/features/controllers/chart_view_window.py
@@ -195,8 +195,8 @@ class _SentimentIntensitySpectrum(QWidget):
             self.rect().adjusted(10, groove_rect.bottom() + 8, -10, 0),
             Qt.AlignHCenter | Qt.AlignTop,
             (
-                f"Love quotient: {self.positive_intensity()}    "
-                f"Hate quotient: {self.negative_intensity()}"
+                f"Frustration: {self.negative_intensity()}"
+                f"Enjoyment: {self.positive_intensity()}    "
             ),
         )
 
